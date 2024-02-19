b67bf013750535d3dbd865b3f05aa7c9720b78fe
ljx <919769034@qq.com>
2024-02-19 22:02:54 +0800

    微信端商品浏览功能代码开发

--- sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
+++ sky-server/src/main/java/com/sky/mapper/SetmealDishMapper.java
@@ -1,5 +1,9 @@
 package com.sky.mapper;
 
+import com.sky.annotation.AutoFill;
+import com.sky.entity.Setmeal;
+import com.sky.entity.SetmealDish;
+import com.sky.enumeration.OperationType;
 import org.apache.ibatis.annotations.Mapper;
 
 import java.util.List;
@@ -8,4 +12,10 @@ import java.util.List;
 public interface SetmealDishMapper {
 
     List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
+    /**
+     * 批量保存套餐和菜品的关联关系
+     * @param setmealDishes
+     */
+    void insertBatch(List<SetmealDish> setmealDishes);
+
 }
